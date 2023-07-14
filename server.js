6e942420d12137a153ee7ed83063bd9ccadc7e0a
pramaths <103276292+pramaths@users.noreply.github.com>
2023-07-15 00:06:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ const isAuthenticated = (req, res, next) => {
     return next();
   } else {
     // User is not authenticated
-    res.redirect("/")
+    res.redirect("/")}
 };
 app.post('/api/companies',async (req, res) => {
   try {
